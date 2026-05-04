23f16e2b40f2b88be9eab8b9fcc79599503a9d64
Megh Patel <M3ghpatel2008@gmail.com>
2026-05-04 08:47:30 -0400

    Home_uptade

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    private Game_Library gameLibrary;
+    private Singleplayer singleplayer;
+    private Multiplayer multiplayer;
+    private game game;
+    public Main()
+    {
+        this.gameLibrary = gameLibrary;
+        
+    }
+}
--- game.java
+++ game.java
@@ -9,8 +9,15 @@ public class game
 {
     private Genre genre;
     private Platform platform;
-    private AgeRating agerating;
+    private AgeRating ageRating;
+    private String name;
+    private int gamesId;
     public game()
     {
+        this.name     = name;
+        this.gamesId  = gamesId;
+        this.ageRating = ageRating;
+        this.genre    = genre;
+        this.platform = platform;
     }
 }
