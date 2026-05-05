74275c574b5369fd49a02517841e12f99178663f
Megh-patel-2008 <M3ghpatel2008@gmail.com>
2026-05-04 23:19:08 -0400

    quater way through main

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  * Write a description of class Main here.
  *
@@ -7,12 +8,28 @@
  */
 public class Main
 {
-    private Game_Library gameLibrary;
-    private Singleplayer singleplayer;
-    private Multiplayer multiplayer;
-    private game game;
-    public Main()
+    static Scanner scanner = new Scanner(System.in);
+    static Game_Library library = new Game_Library();
+    public static void main(String[] args)
     {
-        this.gameLibrary = gameLibrary;
+        System.out.println("=================================");
+        System.out.println("   Welcome to the Game Library   ");
+        System.out.println("=================================");
+        boolean keepGoing = true;
+        while(keepGoing)
+        {
+            System.out.println("What type of game are you looking for?");
+            System.out.println("1. Single Player");
+            System.out.println("2. Multi Player");
+            System.out.println("0. Exit");
+            int typeChoice = readInt("Your choice: ");
+        }
+    }
+    static int readInt(String prompt){
+        System.out.print(prompt);
+        while(!scanner.hasNextInt()){
+            System.out.print("Please enter a number");
+        }
+        return scanner.nextInt();
     }
 }
