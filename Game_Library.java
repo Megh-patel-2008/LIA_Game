6deea9bb8e5a368505db6dd8865544b78eac44f8
Megh-patel-2008 <M3ghpatel2008@gmail.com>
2026-05-11 18:45:26 -0400

    error fix

--- Game_Library.java
+++ Game_Library.java
@@ -27,7 +27,8 @@ public class Game_Library
         games.add(new Singleplayer("Five Nights at Freddy's", 9, AgeRating.TEEN, Genre.HORROR, Platform.MOBILE, true, "Hard"));
         games.add(new Multiplayer("FIFA Mobile", 10, AgeRating.CHILD, Genre.SPORTS, Platform.MOBILE,2));
         games.add(new Multiplayer("Clash of Clans", 11, AgeRating.TEEN, Genre.STRATEGY, Platform.MOBILE, 50));
-        games.add(new Multiplayer("Among Us", AgeRating.CHILD, Genre.STRATEGY,Platform.MOBILE, 10));
+        games.add(new Multiplayer("Among Us", 12, AgeRating.CHILD, Genre.STRATEGY,Platform.MOBILE, 10));
+        //you forgot to add the number 12
         games.add(new Singleplayer("God of War", 13, AgeRating.ADULT, Genre.RPG, Platform.PLAYSTATION, true, "Medium"));
         
     }
