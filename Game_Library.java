b2159b9951f4bec29d93a52f52082c092283afdd
Chester Tuquero <chestertuquero11@gmail.com>
2026-05-11 19:07:52 -0400

    a

--- Game_Library.java
+++ Game_Library.java
@@ -28,9 +28,21 @@ public class Game_Library
         games.add(new Multiplayer("FIFA Mobile", 10, AgeRating.CHILD, Genre.SPORTS, Platform.MOBILE,2));
         games.add(new Multiplayer("Clash of Clans", 11, AgeRating.TEEN, Genre.STRATEGY, Platform.MOBILE, 50));
         games.add(new Multiplayer("Among Us", 12, AgeRating.CHILD, Genre.STRATEGY,Platform.MOBILE, 10));
-        //you forgot to add the number 12
-        games.add(new Singleplayer("God of War", 13, AgeRating.ADULT, Genre.RPG, Platform.PLAYSTATION, true, "Medium"));
-        
+        games.add(new Singleplayer("God of War", 13, AgeRating.ADULT, Genre.RPG, Platform.PLAYSTATION, true, "Hard"));
+        games.add(new Singleplayer("Silent Hill 2", 14, AgeRating.ADULT, Genre.HORROR, Platform.PLAYSTATION, true, "Medium"));        
+        games.add(new Singleplayer("EA FC 25", 15, AgeRating.TEEN, Genre.SPORTS, Platform.PLAYSTATION, true, "Medium"));
+        games.add(new Multiplayer("NBA 2K25", 16, AgeRating.TEEN, Genre.SPORTS, Platform.PLAYSTATION, 4));
+        games.add(new Multiplayer("Diablo IV", 17, AgeRating.ADULT, Genre.RPG, Platform.PLAYSTATION, 4));
+        games.add(new Multiplayer("Predator Hunting Grounds", 18, AgeRating.ADULT, Genre.HORROR, Platform.PLAYSTATION, 5));
+        games.add(new Singleplayer("Halo Infinite", 19, AgeRating.TEEN, Genre.STRATEGY, Platform.XBOX, true, "Medium"));
+        games.add(new Singleplayer("Alan Wake 2", 20, AgeRating.ADULT, Genre.HORROR, Platform.XBOX, true, "Hard"));
+        games.add(new Singleplayer("Fable", 21, AgeRating.TEEN, Genre.RPG, Platform.XBOX, true, "Easy"));
+        games.add(new Multiplayer("Rocket League", 22, AgeRating.CHILD, Genre.SPORTS, Platform.XBOX, 4));
+        games.add(new Multiplayer("Back 4 Blood", 23, AgeRating.ADULT, Genre.HORROR, Platform.XBOX, 4));
+        games.add(new Multiplayer("Gears 5", 24, AgeRating.ADULT, Genre.STRATEGY, Platform.XBOX, 4));
+        games.add(new Singleplayer("Zelda Tears of the Kingdom", 25, AgeRating.CHILD, Genre.RPG, Platform.NINTENDO, true, "Medium"));
+        games.add(new Singleplayer("Fire Emblem Engage", 26, AgeRating.TEEN, Genre.STRATEGY, Platform.NINTENDO, true, "Hard"));
+
     }
     public ArrayList<game> filter(boolean isSinglePlayer, Platform platform, Genre genre, AgeRating ageRating){
         ArrayList<game> result = new ArrayList<>();
